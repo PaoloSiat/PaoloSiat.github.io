dfe9bd1fd16b91df6969bb6f18cb4ff978b0d8ac
Sean Ryan <email@syri.moe>
2020-08-14 19:48:44 +1200

    added fullscreen navigation

--- src/components/fullscreenImage.tsx
+++ src/components/fullscreenImage.tsx
@@ -11,10 +11,14 @@ interface IFullscreenImageState {}
 
 export default class FullscreenImage extends Component<IFullscreenImageProps, IFullscreenImageState> {
   render(): JSX.Element {
+    let path = `images/${this.props.parent.state.fullscreenCurrentCategoryData?.name}/${this.props.parent.state.fullscreenCurrentCategoryData?.images[this.props.parent.state.fullscreenImageIndex]}`;
+
     return(
-      <div id='fullscreen-image' onClick={() => {this.props.parent.setState({ currentFullscreenImage: '' })}} style={{ display: this.props.parent.state.currentFullscreenImage !== '' ? 'flex' : 'none' }}>
+      <div id='fullscreen-image' onClick={() => {this.props.parent.setState({ fullscreenCurrentCategoryData: null, fullscreenImageIndex: 0 })}} style={{ display: this.props.parent.state.fullscreenCurrentCategoryData !== null ? 'flex' : 'none' }}>
         <div id='fullscreen-close'>×</div>
-        <Image src={this.props.parent.state.currentFullscreenImage} alt={`fullscreen portfolio image for: ${this.props.parent.state.currentFullscreenImage}`} fluid={true} draggable={false} />
+        <div className='fullscreen-arrow' onClick={(event) => { event.stopPropagation(); this.props.parent.openFullscreenImage(this.props.parent.state.fullscreenCurrentCategoryData, this.props.parent.state.fullscreenImageIndex - 1) }}><i className='fas fa-angle-double-left'></i></div>
+        <Image src={path} alt={`fullscreen portfolio image for: ${path}`} fluid={true} draggable={false} onClick={(event: any) => { event.stopPropagation(); }} />
+        <div className='fullscreen-arrow' onClick={(event) => { event.stopPropagation(); this.props.parent.openFullscreenImage(this.props.parent.state.fullscreenCurrentCategoryData, this.props.parent.state.fullscreenImageIndex + 1) }}><i className='fas fa-angle-double-right'></i></div>
       </div>
     );
   }
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -8,7 +8,8 @@ interface PortfolioProps {
   siteData: ISiteData;
 }
 interface PortfolioState {
-  currentFullscreenImage: string;
+  fullscreenImageIndex: number;
+  fullscreenCurrentCategoryData: IPortfolioCategory | null;
 }
 
 export default class Portfolio extends Component<PortfolioProps, PortfolioState> {
@@ -18,13 +19,18 @@ export default class Portfolio extends Component<PortfolioProps, PortfolioState>
 
     // Set default data.
     this.state = {
-      currentFullscreenImage: ''
+      fullscreenImageIndex: 0,
+      fullscreenCurrentCategoryData: null
     }
   }
 
   // Creates a full screen image element that shows on top of the site.
-  openFullscreenImage(imagePath: string): void {
-    this.setState({ currentFullscreenImage: imagePath });
+  openFullscreenImage(categoryData: IPortfolioCategory | null, index: number): void {
+    if(categoryData) {
+      index = Math.min(Math.max(index, 0), categoryData?.images.length - 1);
+
+      this.setState({ fullscreenCurrentCategoryData: categoryData, fullscreenImageIndex: index });
+    }
   }
 
   // This function makes it possible to create a section of HTML code that is reusable and uses the category parameter to change content.
@@ -36,16 +42,17 @@ export default class Portfolio extends Component<PortfolioProps, PortfolioState>
     for (let categoryData of this.props.siteData.portfolioCategories) {
       if (category === categoryData.name) {
         // Loop through the images that are in the matched category to create and add them to the images list.
-        for (let image of categoryData.images) {
-          let path = `images/${categoryData.name}/${image}`;
-
+        for (let image in categoryData.images) {
+          let path = `images/${categoryData.name}/${categoryData.images[image]}`;
+          
           // Create an image element using using the information from the data and bind a click event to it.
           images.push(
-            <Image key={path} src={path} alt={`portfolio image for: ${path}`} className='portfolio-image' fluid={true} draggable={false} onClick={() => this.openFullscreenImage(path)} />
+            <Image key={path} src={path} alt={`portfolio image for: ${path}`} className='portfolio-image' fluid={true} draggable={false} onClick={() => this.openFullscreenImage(categoryData, parseInt(image))} />
           );
         }
 
-        result.push(<div key='selector' className='portfolio-selector'  onClick={() => {
+        result.push(
+        <div key='selector' className='portfolio-selector'  onClick={() => {
           let element = document.getElementById(`portfolio-content-${categoryData.name}`);
 
           if (categoryData.visible && element) element.style.display = 'none';
@@ -58,7 +65,8 @@ export default class Portfolio extends Component<PortfolioProps, PortfolioState>
           <div>
             {categoryData.visible ? '▲' : '▼'}
           </div>
-        </div>);
+        </div>
+        );
 
         // Wrap the images in a container element and add it to the result.
         result.push(<div key='images' id={`portfolio-content-${categoryData.name}`} className='portfolio-content' style={{ display: 'none' }}>{images}</div >);
